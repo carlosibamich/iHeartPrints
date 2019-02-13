1f7dbac978765c1b403fbcf912b595a8d12aef45
Carlos Ibarra <carlosibarra.art@gmail.com>
2019-02-13 16:54:10 -0700

    Added a loop to carousel indicators to dynamically match the number of slides. Added style modifications to landing page and products/index.html.erb

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,7 +4,7 @@ class SimplePagesController < ApplicationController
   end
 
   def landing_page
-    @products = Product.limit(3)
+    @products = Product.limit(5)
     @featured_product = Product.first
   end
 end
