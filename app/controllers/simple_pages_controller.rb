8364c2b08bccfe472e4673ab596d12cced73c6dd
Carlos Ibarra <carlosibarra.art@gmail.com>
2019-03-04 17:10:01 -0700

    5.3 Created a contact form in the Contact Page. Created thank_you file in simple_pages directory. On form submit user is directed to thank_you view. Configured email submit to send admin an email notice.

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,10 +1,20 @@
 class SimplePagesController < ApplicationController
   def index
-    
   end
 
   def landing_page
     @products = Product.limit(3)
-    @featured_product = Product.first
+    #@featured_product = Product.first
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'carlosibamich@hotmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
+
 end
