e9a47db90dd3044cde8a115b5039d4ce7abd3a20
Carlos Ibarra <carlosibarra.art@gmail.com>
2019-05-04 13:08:07 -0700

    6.6 Generated a PaymentsController with an action create. Created a Stripe charge object. Added a payment form to products/show.

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,15 @@ Rails.application.routes.draw do
   resources :products do
     resources :comments
   end
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-  root 'products#index'
   post 'simple_pages/thank_you'
+  root 'products#index'
+  post 'payments/create'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
