ad5d47124cfbb1e362d4199e83b622158ab13f8c
Carlos Ibarra <carlosibarra.art@gmail.com>
2019-05-31 03:10:50 -0700

    6.14 Improved ActionCable performance and added ActiveJob.

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -9,8 +9,22 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-    receivedComment = $('.alert.alert-info');
+    console.log(data.comment.body);
+    receivedComment = $('.alert-info');
     receivedComment.show();
     receivedComment.delay(2400).fadeOut(800);
+    $('.ajax-comment-response').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
+  },
+
+  listen_to_comments: function() {
+    return this.perform('listen', {
+      product_id: $('[data-product-id]').data('product-id')
+    });
   }
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
